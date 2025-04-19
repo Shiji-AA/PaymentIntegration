0559b1bc37c8d7e168493fce2b7e96de56e54ca2
shijiaa <shijihcl@gmail.com>
2025-04-19 13:38:01 +0530

    chages added

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -22,11 +22,18 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// const corsOptions = {
+//   origin: ['http://localhost:4000', 'https://courses.arcite.in', 'https://www.courses.arcite.in', 'http://localhost:3000'],
+//   methods: "GET, PUT, POST, PATCH, DELETE",
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// };
+
 const corsOptions = {
   origin: ['http://localhost:4000', 'https://courses.arcite.in', 'https://www.courses.arcite.in', 'http://localhost:3000'],
   methods: "GET, PUT, POST, PATCH, DELETE",
   allowedHeaders: ["Content-Type", "Authorization"]
 };
+
 app.use(cors(corsOptions));
 
 
@@ -34,6 +41,8 @@ app.use("/api/users", userRoutes);
 app.use('/api/admin', adminRouter);
 
 app.use(express.static(join(__dirname, '../../frontend/dist')));
+
+
 app.get('*', (req, res) => {
   res.sendFile(join(__dirname, "../../frontend/dist/index.html"));
 });
