02348523f93515505f5c07facc52d15b72b843a0
shijiaa <shijihcl@gmail.com>
2025-04-21 11:09:13 +0530

    ch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+//     host: 'localhost',
+//     port: 4000
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     host: 'localhost',
     port: 4000
-  }
+  },
+  build: {
+    outDir: 'dist', // default is fine unless you changed it
+  },
+ 
 })
