bff143ab469cedfc4632292290e74d42f804b5dd
shijiaa <shijihcl@gmail.com>
2025-04-21 11:27:53 +0530

    cha

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,48 +1,57 @@
 
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
 import cors from "cors";
-import userRoutes from "./Routes/userRoutes/userRoutes.js";
-import adminRouter from "./Routes/adminRoutes/adminRoutes.js";
-import { connectDB } from "./config/db.js";
+import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import { connectDB } from './config/db.js';
+import userRoutes from "./Routes/userRoutes/userRoutes.js";
+import adminRouter from "./Routes/adminRoutes/adminRoutes.js";
+
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+// Define __dirname for ES module compatibility
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const port = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: ['http://localhost:4000', 'https://courses.arcite.in', 'https://www.courses.arcite.in', 'http://localhost:3000'],
-  methods: "GET, PUT, POST, PATCH, DELETE",
-  allowedHeaders: ["Content-Type", "Authorization"]
-};
-
-
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(join(__dirname, '../../frontend/dist')));
+const corsOptions = {
+  origin: ['http://localhost:4000', 'https://courses.arcite.in', 'https://courses.arcite.in', 'http://localhost:3000'],
+  methods: "GET, PUT, POST, PATCH, DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
+app.use(cors(corsOptions));
 
 app.use("/api/users", userRoutes);
 app.use('/api/admin', adminRouter);
 
+// Serving static files
+app.use(express.static(join(__dirname, '../../frontend/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(join(__dirname, "../../frontend/dist/index.html"));
 });
 
-app.get("/", (req, res) => res.send("Server is ready"));
+app.get('/', (req, res) => res.send("server is ready"));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  console.log("server started")
+);
 
 
- 
+
+
+
+
+
 
 
 
