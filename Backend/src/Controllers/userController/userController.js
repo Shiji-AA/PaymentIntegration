55eba859d26c3573bf488d6cec9c97545a920104
shijiaa <shijihcl@gmail.com>
2025-08-28 12:03:14 +0530

    Google Authentication added

--- Backend/src/Controllers/userController/userController.js
+++ Backend/src/Controllers/userController/userController.js
@@ -70,6 +70,67 @@ const loginUser = async (req, res) => {
     }
   };
 
+
+  const googleRegister = async (req, res) => {
+  try {
+    const token = req.body.credential;
+    const decodedData = jwt.decode(token);
+    if (!decodedData) {
+      return res.status(400).json({ error: "Invalid token" });
+    }
+    const { name, email, picture, jti } = decodedData;
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+    user = new User({
+      name,
+      email,
+      photo: picture,
+      password: jti, // Consider not using jti as a password
+    });
+    await user.save();
+    res.status(200).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+
+
+// Google Login
+const googleLogin = async (req, res) => {
+  try {
+      const decodedData = jwt.decode(req.body.credential);
+      if (!decodedData) {
+          return res.status(400).json({ error: "Invalid credentials" });
+      }
+
+      const { name, email, picture, jti } = decodedData;
+      const user = await User.findOne({ email });
+
+      if (user) {
+          const token = generateToken(user._id);
+          const userData = {
+              name: user.name,
+              email: user.email,
+              id: user._id,
+              phone: user.phone,
+              photo: user.photo,
+          };
+          return res.json({
+              userData,
+              token,
+              message: "Success",
+          });
+      } else {
+          return res.status(401).json({ error: "Invalid Email and Password" });
+      }
+  } catch (error) {
+      res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
   const getAllCources1 = async (req, res) => {
       try {
         const departmentName = req.query.department;
@@ -128,5 +189,5 @@ const loginUser = async (req, res) => {
 
 
   
-  export { registerUser ,loginUser,getAllCources1,getCourseById1};
+  export { registerUser ,loginUser,getAllCources1,getCourseById1,googleLogin,googleRegister};
   
